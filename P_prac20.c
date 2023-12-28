4a9fb7416de1a99b07b0f9a2a301e74377a6cc1f
aggarwalom1212 <151609714+aggarwalom1212@users.noreply.github.com>
2023-12-28 15:05:13 +0530

    Update P_prac20.c

--- P_prac20.c
+++ P_prac20.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
-    void swap(int *a, int *b)
-        {
-            int temp = *a;
-            *a = *b;
-            *b = temp;
-        }
-    
-    int main() 
-        {
-            int num1, num2;
-            printf("Enter value for num1: ");
-            scanf("%d", &num1);
-            printf("Enter value for num2: ");
-            scanf("%d", &num2);
-            printf("Before swapping: num1 = %d, num2 = %d\n", num1, num2);
-            swap(&num1, &num2);
-            printf("After swapping: num1 = %d, num2 = %d\n", num1, num2);
-            return 0;
-        }
+        struct Employee 
+                {
+                      int empId;
+                      char empName[50];
+                      float empSalary;
+                };
+        
+        void main() 
+                {
+                        int numEmployees = 3;
+                        struct Employee employees[numEmployees];
+                        for (int i = 0; i < numEmployees; ++i) 
+                                {
+                                      printf("Enter details for Employee %d:\n", i + 1);
+                                      printf("ID: ");
+                                      scanf("%d", &employees[i].empId);
+                                      printf("Name: ");
+                                      scanf("%s", employees[i].empName);
+                                      printf("Salary: ");
+                                      scanf("%f", &employees[i].empSalary);
+                                      printf("\n");
+                                }
+                        printf("Employee Database:\n");
+                        for (int i = 0; i < numEmployees; ++i)
+                                {
+                                      printf("Employee %d\n", i + 1);
+                                      printf("ID: %d\n", employees[i].empId);
+                                      printf("Name: %s\n", employees[i].empName);
+                                      printf("Salary: %.2f\n", employees[i].empSalary);
+                                      printf("\n");
+                                }
+                }
