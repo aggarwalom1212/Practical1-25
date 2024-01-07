c9b3ea6ddcd230deadd2c7a9af3cce4edaafb0bb
aggarwalom1212 <151609714+aggarwalom1212@users.noreply.github.com>
2024-01-07 20:44:27 +0530

    Update P_prac2.c

--- P_prac2.c
+++ P_prac2.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
-    float cel_to_far(float celcius)
+    float cel_to_far(float celc)
         {
-	          return (celcius * (9/5) + 32);
+	          return (celc * (9/5) + 32);
         }
-    float far_to_cel(float farenhiet)
+    float far_to_cel(float far)
         {
-	          return (celcius * (5/9) + 32);
+	          return ((far-32)*(5/9));
         }
     void main()
         {
-	          float celcius;float farenhiet;
+	          float celc;
+		  float far;
 	          printf("Enter temperature in celcius: ");
-	          scanf("%f",celcius);
-	          printf("entered temperaturein farenhiet: %f",cel_to_far(celcius))
+	          scanf("%f",celc);
+	          printf("entered temperaturein farenhiet: %f",cel_to_far(celc))
 	          printf('Enter temperature in farenhiet: ');
-	          scanf("%f",farenhiet);
-	          printf("entered temperaturein farenhiet: %f",far_to_cel(farenhiet))
+	          scanf("%f",far);
+	          printf("entered temperaturein farenhiet: %f",far_to_cel(far))
         }
