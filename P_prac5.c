c226554cef3c5e98591edad3a5b32766ae0168a2
aggarwalom1212 <151609714+aggarwalom1212@users.noreply.github.com>
2024-01-08 01:34:34 +0530

    Update P_prac5.c

--- P_prac5.c
+++ P_prac5.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
     int main()
         {
-            int number;
-            long result;
+            int n;
+            long result=1;
             printf("Enter a number to calculate its factorial: ");
-            scanf("%d", &number);
-            if (number < 0) 
+            scanf("%d", &n);
+            if (n < 0) 
                 {
                     printf("Factorial is not defined for negative numbers.\n");
                 } 
             else 
                 {
-                      long fact = 1;
+                      
                       for (int i = 1; i <= n; i++) 
-                      fact *= i;
+                      result = result *i;
                 }
-            printf("Factorial of %d is %ld\n", number, result);
+            printf("Factorial of %d is %ld\n", n, result);
         }
