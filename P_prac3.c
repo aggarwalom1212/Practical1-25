2cf1d4dd7f0212ed008d755b58959348ecc1b926
aggarwalom1212 <151609714+aggarwalom1212@users.noreply.github.com>
2024-01-07 21:13:11 +0530

    Update P_prac3.c

--- P_prac3.c
+++ P_prac3.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-    void main()
+    int main()
         {
           	int sum=0;
           	int n;
-          	printf("enter the number to which sum is to be calculated: %d ",n);
+          	printf("enter the number to which sum is to be calculated: ");
+            scanf("%d", &n);
           	for(int i=0;i<=n;i++)
                 {
-          		      sum = sum +i;
+          	         sum = sum +i;
           	    }
           	printf("sum of numbers:%d",sum);
+            return 0;
         }
+
