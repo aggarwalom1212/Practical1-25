a5ad6db1fc3432fbb0e2808f4ab15dfcbe4d231a
aggarwalom1212 <151609714+aggarwalom1212@users.noreply.github.com>
2024-01-08 09:44:25 +0530

    Create P_prac9(B).c

---
+++ P_prac9(B).c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+    void swap(int *a, int *b)
+        {
+            if(*a > *b)
+                {
+                    *a= *a- *b;
+                    *b= *a+ *b;
+                    *a= *b- *a;
+                }
+            else
+                {
+                    *b= *a- *b;
+                    *a= *b+ *a;
+                    *b= *a- *b;
+                }
+        }    
+    int main() 
+        {
+            int num1, num2;
+            printf("Enter value for num1: ");
+            scanf("%d", &num1);
+            printf("Enter value for num2: ");
+            scanf("%d", &num2);
+            printf("Before swapping: num1 = %d, num2 = %d\n", num1, num2);
+            swap(&num1, &num2);
+            printf("After swapping: num1 = %d, num2 = %d\n", num1, num2);
+            return 0;
+        }
